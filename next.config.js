c2fe3cd09826b24198112f5b18041313cfc9367f
D3Portillo <d3portillo@gmail.com>
2022-12-08 05:19:15 +0100

    chores(next.config.js): add redirect to litepaper

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/litepaper",
+        destination: "https://coda.io/d/_dZgc8tByNF2/Eliptico-Litepaper_suUIv",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
