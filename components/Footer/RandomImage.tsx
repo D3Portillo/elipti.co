adcec186d7b91fd3e67428e375f807f8fcdfb2d0
D3Portillo <d3portillo@gmail.com>
2022-12-11 21:11:39 +0100

    fix(RandomImage.tsx): make the image src actually random on prod D:

--- components/Footer/RandomImage.tsx
+++ components/Footer/RandomImage.tsx
@@ -1,5 +1,5 @@
-import { useMemo } from "react"
-import Image from "next/image"
+import { useLayoutEffect, useState } from "react"
+import Image, { StaticImageData } from "next/image"
 
 import asset_image_1 from "@/assets/unsplash/engin-akyurt.jpg"
 import asset_image_2 from "@/assets/unsplash/velizar-ivanov.jpg"
@@ -8,14 +8,20 @@ import asset_image_3 from "@/assets/unsplash/zoran-borojevic.jpg"
 const IMAGES = [asset_image_1, asset_image_2, asset_image_3]
 
 function RandomImage() {
-  const renderImage = useMemo(() => {
-    const randomIdx = Math.ceil(IMAGES.length * Math.random())
-    return IMAGES[randomIdx]
+  const [asset, setAsset] = useState<StaticImageData>()
+
+  useLayoutEffect(() => {
+    setAsset(() => {
+      const randomIdx = Math.floor(IMAGES.length * Math.random())
+      return IMAGES[randomIdx]
+    })
   }, [])
+
+  const blurData = asset?.blurDataURL || asset_image_2.blurDataURL
 
   return (
     <figure className="relative w-full md:max-w-md">
-      <Image placeholder="blur" src={renderImage || asset_image_2} alt="" />
+      <Image blurDataURL={blurData} placeholder="blur" src={asset!} alt="" />
     </figure>
   )
 }
